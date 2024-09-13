173ec84837c8b29dc4e08c7418637f3d72f74472
Mehandi <mehandixaidi786@gmail.com>
2024-09-14 04:29:08 +0530

    Sidebar

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,38 @@
+import './Main.css'
+import { assets } from '../../assets/assets'
+const Main = () => {
+  return (
+    <div className="main">
+        <div className="nav">
+            <p>Gemini</p>
+            <img src={assets.user_icon} alt="" />
+        </div>
+        <div className="main-container">
+            <div className="greet">
+                <p><span>Hello, Syed.</span></p>
+                <p>How can i help you today?</p>
+            </div>
+            <div className="cards">
+                <div className="card">
+                    <p>Suggest a beautifull place on the upcomming road trip</p>
+                    <img src={assets.compass_icon} alt="" />
+                </div>
+                <div className="card">
+                    <p>Briefly summerize this concept: urban planning</p>
+                    <img src={assets.bulb_icon} alt="" />
+                </div>
+                <div className="card">
+                    <p>Brain Storm team bonding for our work retreat</p>
+                    <img src={assets.message_icon} alt="" />
+                </div>
+                <div className="card">
+                    <p>Improve the readibiality of the following code</p>
+                    <img src={assets.code_icon} alt="" />
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Main
