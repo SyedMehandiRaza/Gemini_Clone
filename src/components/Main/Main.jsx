c1e70c18b4c3b74fbaab332dcefaf46ce641fa8a
Mehandi <mehandixaidi786@gmail.com>
2024-09-19 04:06:07 +0530

    ui of gemini

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,38 +1,52 @@
-import './Main.css'
-import { assets } from '../../assets/assets'
+import "./Main.css";
+import { assets } from "../../assets/assets";
 const Main = () => {
   return (
     <div className="main">
-        <div className="nav">
-            <p>Gemini</p>
-            <img src={assets.user_icon} alt="" />
+      <div className="nav">
+        <p>Gemini</p>
+        <img src={assets.user_icon} alt="" />
+      </div>
+      <div className="main-container">
+        <div className="greet">
+          <p>
+            <span>Hello, Syed.</span>
+          </p>
+          <p>How can i help you today?</p>
         </div>
-        <div className="main-container">
-            <div className="greet">
-                <p><span>Hello, Syed.</span></p>
-                <p>How can i help you today?</p>
-            </div>
-            <div className="cards">
-                <div className="card">
-                    <p>Suggest a beautifull place on the upcomming road trip</p>
-                    <img src={assets.compass_icon} alt="" />
-                </div>
-                <div className="card">
-                    <p>Briefly summerize this concept: urban planning</p>
-                    <img src={assets.bulb_icon} alt="" />
-                </div>
-                <div className="card">
-                    <p>Brain Storm team bonding for our work retreat</p>
-                    <img src={assets.message_icon} alt="" />
-                </div>
-                <div className="card">
-                    <p>Improve the readibiality of the following code</p>
-                    <img src={assets.code_icon} alt="" />
-                </div>
-            </div>
+        <div className="cards">
+          <div className="card">
+            <p>Suggest a beautifull place on the upcomming road trip</p>
+            <img src={assets.compass_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Briefly summerize this concept: urban planning</p>
+            <img src={assets.bulb_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Brain Storm team bonding for our work retreat</p>
+            <img src={assets.message_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Improve the readibiality of the following code</p>
+            <img src={assets.code_icon} alt="" />
+          </div>
         </div>
+        <div className="main-bottom">
+          <div className="search-box">
+            <input type="text" placeholder="Enter a promp here..." />
+            <img src={assets.gallery_icon} alt="" />
+            <img src={assets.mic_icon} alt="" />
+            <img src={assets.send_icon} alt="" />
+          </div>
+        </div>
+        <p className="bottom-info">
+          Suggest a beautifull place on the upcomming road trip Briefly
+          summerize this concept urban planning
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
---
+++ src/config/gemini.js
@@ -0,0 +1,46 @@
+
+
+const apikey = "-eSclGLJ70alM";
+
+
+/*
+ * Install the Generative AI SDK
+ *
+ * $ npm install @google/generative-ai
+ */
+
+const {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+  } = require("@google/generative-ai");
+  
+  const apiKey = "AIzaSyDsSbKBoVphsC05YG_yvN";
+  const genAI = new GoogleGenerativeAI(apiKey);
+  
+  const model = genAI.getGenerativeModel({
+    model: "gemini-1.5-flash",
+  });
+  
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 64,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+  };
+  
+  async function run() {
+    const chatSession = model.startChat({
+      generationConfig,
+   // safetySettings: Adjust safety settings
+   // See https://ai.google.dev/gemini-api/docs/safety-settings
+      history: [
+      ],
+    });
+  
+    const result = await chatSession.sendMessage("INSERT_INPUT_HERE");
+    console.log(result.response.text());
+  }
+  
+  run();
